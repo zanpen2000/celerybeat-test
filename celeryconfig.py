dd0c973181455655be6b83619f02a1ebb2554984
peng <zanpen2000@gmail.com>
2015-08-31 18:22:36 +0800

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -6,17 +6,9 @@ CELERY_MONGODB_SCHEDULER_URL = "mongodb://192.168.2.123:27017"
 
 
 CELERYBEAT_SCHEDULE = {
-    'add-task':{
-        'task':'tasks.add',
-        'schedule':10,
-        'args':(100,100),
-        'kwargs':{'taskid':'123'},
-    },
     'test-task':{
         'task':'tasks.test',
         'schedule':10,
-        'args':(),
-        'kwargs':{},
     }
 
 }
